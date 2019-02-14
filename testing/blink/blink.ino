b6b29fbecf4b6c6eef266951890e645f5c59bd4d
kayjean <kayjean@gmail.com>
2019-02-14 17:47:38 +0800

    correct context and control many leds
    1.copy wrong project, replace it
    2.control three leds, not only one

--- testing/blink/blink.ino
+++ testing/blink/blink.ino
@@ -1,18 +1,37 @@
-int ledPin = 13; // 把 LED 接上 PWM pin3
-int ledPin2 = 12; // 把 LED 接上 PWM pin3
-void setup()
-{
-pinMode(ledPin, OUTPUT); // 設定 pin 3 為輸出
-pinMode(ledPin2, OUTPUT); // 設定 pin 3 為輸出
+/*
+  Blink
+
+  Turns an LED on for one second, then off for one second, repeatedly.
+
+  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
+  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
+  the correct LED pin independent of which board is used.
+  If you want to know what pin the on-board LED is connected to on your Arduino
+  model, check the Technical Specs of your board at:
+  https://www.arduino.cc/en/Main/Products
+
+  modified 8 May 2014
+  by Scott Fitzgerald
+  modified 2 Sep 2016
+  by Arturo Guadalupi
+  modified 8 Sep 2016
+  by Colby Newman
+
+  This example code is in the public domain.
+
+  http://www.arduino.cc/en/Tutorial/Blink
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
 }
-// 下面這個 loop 會讓 LED 燈由暗變為一半亮度，最後變成最大亮度
-void loop()
-{
-  for( int i = 0 ; i < 240 ;  )
-  {
-analogWrite(ledPin, i);  // LED 不亮
-analogWrite(ledPin2, i);  // LED 不亮
-delay (200);
-i += 10;
-  }
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                       // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);                       // wait for a second
 }
--- workspace/main/main.ino
+++ workspace/main/main.ino
@@ -1,7 +1,15 @@
+//arduino 1.8.8
+//choose mega ( even it is frearduino V2.0
+//copy two directory to C:\Program Files (x86)\Arduino\libraries
+//USB_Host_Shield and AndroidAccessory
+
+
 #include <Max3421e.h>
  #include <Usb.h>
  #include <AndroidAccessory.h>
  #define  LED_PIN  13
+ #define  LED_PIN2  14
+ #define  LED_PIN3  15
  AndroidAccessory acc("Manufacturer",
    "Model",
    "Description",
@@ -16,6 +24,8 @@
    // set communiation speed
    Serial.begin(115200);
    pinMode(LED_PIN, OUTPUT);
+   pinMode(LED_PIN2, OUTPUT);
+   pinMode(LED_PIN3, OUTPUT);      
    acc.powerOn();
  }
   
@@ -29,13 +39,26 @@
        {
          //digitalWrite(LED_PIN,HIGH); // turn on light
          brightness = 255*msg[1]/100;
-          analogWrite(LED_PIN, brightness);
+         analogWrite(LED_PIN, brightness);
+
+         brightness = 255*msg[2]/100;
+         analogWrite(LED_PIN2, brightness);
+
+         brightness = 255*msg[3]/100;
+         analogWrite(LED_PIN3, brightness);
+         
        }
        else
          digitalWrite(LED_PIN,LOW); // turn off light
+         digitalWrite(LED_PIN2,LOW); // turn off light
+         digitalWrite(LED_PIN3,LOW); // turn off light
      }
    } 
    else
+   {
      digitalWrite(LED_PIN , LOW); // turn off light
+     digitalWrite(LED_PIN2 , LOW); // turn off light
+     digitalWrite(LED_PIN3 , LOW); // turn off light
+   }
  }
  
